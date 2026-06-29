1d91532895597a76d30169cf8ef489898fd955ea
Santu pramanik <santu700141@gmail.com>
2026-06-29 16:50:21 +0530

    fix: herosection slider number not visible in the mobile screen view port

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,11 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 's40091.pcdn.co',
       },
+      // can be removed
+      {
+        protocol: 'https',
+        hostname: 'zzgqcuwemqrnhlkdqgez.supabase.co',
+      },
     ],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
