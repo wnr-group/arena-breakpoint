69d1020357de6d88dc355f6ca8332d4aea272c4b
Adithya <aadhithsd@gmail.com>
2026-07-01 01:50:52 +0530

    fix: add BreakpointLoader import to subscription plan page

--- app/(customer)/subscription/[planId]/page.tsx
+++ app/(customer)/subscription/[planId]/page.tsx
@@ -7,6 +7,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Card } from '@/components/ui/card'
+import { BreakpointLoader } from '@/components/shared/BreakpointLoader'
 import { toast } from 'sonner'
 import { formatDateForDB, formatDateForDisplay, handleDobInput, isValidDateDDMMYYYY } from '@/lib/utils/dates'
 
