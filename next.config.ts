7f86f2cacd623c6a76176cd9e5ef02de3e5c41b1
Santu pramanik <santu700141@gmail.com>
2026-07-01 21:35:43 +0530

    fix: image fetching from DB

--- next.config.ts
+++ next.config.ts
@@ -19,11 +19,6 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: '*.supabase.co',
       },
-      // can be removed
-      {
-        protocol: 'https',
-        hostname: 'zzgqcuwemqrnhlkdqgez.supabase.co',
-      },
     ],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
